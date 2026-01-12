11f3161a482d8bdfe04fe26edd56e5f0fc3d4811
David Soto <ds580999@gmail.com>
2026-01-11 22:27:08 -0800

    Use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
+import axios from 'axios'
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products.js'
 import './homepage.css';
 
 export function HomePage() {
     //Get data from backend
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            response.json()
-        }).then((data) => {
-            console.log(data);
-        });
+            console.log(response.data);
+        })
 
     return (
         <>
