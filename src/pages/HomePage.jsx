a06242b51e08c09643436aa46660437794d31172
David Soto <ds580999@gmail.com>
2026-01-11 23:19:14 -0800

    Lift cart up and chare it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,21 +3,17 @@ import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import './homepage.css';
 
-export function HomePage() {
+export function HomePage({ cart }) {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+
 
     useEffect(() => {
-        //Get data from backend
+        //Get product data from backend
         axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data)
             });
 
-        axios.get('/api/cart-items')
-            .then((response) => {
-                setCart(response.data);
-            });
     }, []);
 
     return (
