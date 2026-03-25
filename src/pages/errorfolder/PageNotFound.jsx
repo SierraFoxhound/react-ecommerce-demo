c1119b7a13525a1b94dc74592ff821a501531d09
David Soto <ds580999@gmail.com>
2026-03-24 17:09:34 -0700

    Add favicon to 404 page

--- src/pages/errorfolder/PageNotFound.jsx
+++ src/pages/errorfolder/PageNotFound.jsx
@@ -7,6 +7,7 @@ export function PageNotFound({ cart }) {
         <>
 
             <title>404 Page Not Found</title>
+            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
             <Header cart={cart} />
 
