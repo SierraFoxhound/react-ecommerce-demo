381ab2cd3d3ba61286748ba0f175b10668024331
David Soto <ds580999@gmail.com>
2026-04-07 16:06:40 -0700

    add / to url

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -23,7 +23,7 @@ export function CheckoutPage({ cart, loadCart }) {
 
     useEffect(() => {
         const fetchPaymentSummary = async () => {
-            const response = await axios.get('api/payment-summary');
+            const response = await axios.get('/api/payment-summary');
             setPaymentSummary(response.data);
         };
         fetchPaymentSummary();
